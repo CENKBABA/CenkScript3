b402530b9891c519a9d88eb20a3ddc785637aad8
CENKBABA <53657418+CENKBABA@users.noreply.github.com>
2025-08-06 14:36:09 +0300

    Add files via upload

--- dosya.py
+++ dosya.py
@@ -2,7 +2,13 @@ import sys
 from cenksp3 import kod_optimize,kod_oku
 
 # print(sys.argv)
-dosya=open(sys.argv[1],"r")
+
+kodlama=""
+if len(sys.argv)>=3 and sys.argv[2].lower()=="türkçe":
+	kodlama="utf-8"
+else:
+	kodlama="ansi"
+dosya=open(sys.argv[1],"r",encoding=kodlama)
 
 kod=""
 
@@ -13,5 +19,7 @@ while len(satır)>0:
 	satır=dosya.readline()
 	
 kod=kod_optimize(kod)
+
+# print("anan",kod)
 
 kod_oku(kod)
